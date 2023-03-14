2b6ab38c5c9af44810ee9af8104ec722282c07b8
Paul Aiamsubhab <paul.aiamsubhab@gmail.com>
2023-03-13 23:37:15 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+
+
+function superbowlWin (array){
+    let result = array.find(isWin => isWin.result === 'W');
+      if (result){
+      return result.year;
+    } else {
+      return undefined;
+    }
+  }
